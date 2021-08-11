8627244efb655f4e488d61839de929d191421e4f
Ubuntu <ubuntu@ip-172-31-17-171.us-east-2.compute.internal>
2021-08-11 05:55:34 +0000

    Dataset upload

---
+++ DataCollection/data.sh
@@ -0,0 +1,33 @@
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/001/SRR3359531/SRR3359531_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/001/SRR3359531/SRR3359531_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/002/SRR3359532/SRR3359532_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/002/SRR3359532/SRR3359532_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/003/SRR3359533/SRR3359533_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/003/SRR3359533/SRR3359533_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/004/SRR3359534/SRR3359534_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/004/SRR3359534/SRR3359534_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/005/SRR3359535/SRR3359535_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/005/SRR3359535/SRR3359535_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/006/SRR3359536/SRR3359536_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/006/SRR3359536/SRR3359536_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/007/SRR3359537/SRR3359537_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/007/SRR3359537/SRR3359537_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/008/SRR3359538/SRR3359538_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/008/SRR3359538/SRR3359538_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/009/SRR3359539/SRR3359539_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/009/SRR3359539/SRR3359539_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/000/SRR3359540/SRR3359540_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/000/SRR3359540/SRR3359540_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/001/SRR3359541/SRR3359541_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/001/SRR3359541/SRR3359541_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/002/SRR3359542/SRR3359542_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/002/SRR3359542/SRR3359542_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/003/SRR3359543/SRR3359543_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/003/SRR3359543/SRR3359543_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/004/SRR3359544/SRR3359544_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/004/SRR3359544/SRR3359544_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/005/SRR3359545/SRR3359545_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR335/005/SRR3359545/SRR3359545_2.fastq.gz
+
+
+
